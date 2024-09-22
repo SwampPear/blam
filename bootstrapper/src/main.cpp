4eacfe1b76b6f2d3b631923efa2e2c2dd13124ef
SwampPear <michaeljosephvaden03@gmail.com>
2024-09-22 16:56:53 -0400

    implemented function to get cwd

--- bootstrapper/src/main.cpp
+++ bootstrapper/src/main.cpp
@@ -1,9 +1,15 @@
-#include <cstdlib>
 #include <iostream>
+#include <cstdlib>
+#include <filesystem>
 
+std::filesystem::path get_cwd() {
+    return std::filesystem::current_path();
+}
 
 int main() {
-    std::cout << "test" << std::endl;
-    
+    std::cout << get_cwd() << std::endl;
+
+    //get_cwd();  // Call the get_cwd function to print the current working directory
+
     return EXIT_SUCCESS;
 }
