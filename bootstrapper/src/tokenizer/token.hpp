40812254d0b5d167609a190edf8bc847d204713a
Michael Vaden <michaelvaden.mjv@gmail.com>
2025-05-23 19:52:11 -0400

    sorted token type list

--- bootstrapper/src/tokenizer/token.hpp
+++ bootstrapper/src/tokenizer/token.hpp
@@ -6,7 +6,7 @@
 #include <map>
 
 
-namespace Tok {
+namespace Tokenizer {
 
 // ordered in lexeme hierarchy first to last
 enum class Type : uint8_t {
@@ -16,57 +16,8 @@ enum class Type : uint8_t {
     MLINE_COMMENT,       // #* *#
     SLINE_COMMENT,       // # \n
 
-    // strings (should be parsed first to account for characters within)
-    STRING,              // "<any char>" (multiline by default)
-
-    // space
-    WHITESPACE,          // \s+
-    NLINE,               // \n (newline)
-
-    // delimeters
-    SMBRACKET_L,         // (
-    SMBRACKET_R,         // )
-    SQBRACKET_L,         // [
-    SQBRACKET_R,         // ]
-    CUBRACKET_L,         // {
-    CUBRACKET_R,         // }
-
-    // operators that need parsed before arithmetic oerators
-    ARROW,               // -> 
-    BAND,                // &
-    BOR,                 // |
-    BXOR,                // ^
-    BLS,                 // <<
-    BRS,                 // >>
-
-    // arithmetic
-    EQ,                  // =
-    PLUS,                // +
-    MIN,                 // -
-    DIV,                 // /
-    EXP,                 // **
-    MULT,                // * 
-    PLUSEQ,              // +=
-    MINEQ,               // -=
-    DIVEQ,               // /=
-    MULTEQ,              // *=
-
-    // comparison
-    EQEQ,                // ==
-    NEQ,                 // !=
-    LT,                  // <
-    GT,                  // >
-    LTE,                 // <=
-    GTE,                 // >=
-
-    // decimal (should parse before because of dot)
-    DECIMAL,             // 0.234
-
-    // operators
-    DOT,                 // .  
-    COMMA,               // ,
-    COLON,               // :
-    AT,                  // @
+    // strings
+    STRING,              // "example"
 
     // reserved keywords
     AND,                 // and
@@ -85,11 +36,55 @@ enum class Type : uint8_t {
     FALSE,               // false
     NIL,                 // nil
 
-    // text (any other text later determined to be a type, class, e.t.c.)
-    TEXT,
+    TEXT,                // any text
 
-    // number (parsed last because of numbers being in text)
+    // numbers
+    DECIMAL,             // 0.234
     NUMBER,              // 123
+
+    // space
+    WHITESPACE,          // \s+
+    NLINE,               // \n (newline)
+
+    // delimeters
+    SMBRACKET_L,         // (
+    SMBRACKET_R,         // )
+    SQBRACKET_L,         // [
+    SQBRACKET_R,         // ]
+    CUBRACKET_L,         // {
+    CUBRACKET_R,         // }
+
+    // operators
+    ARROW,               // -> 
+    BAND,                // &
+    BOR,                 // |
+    BXOR,                // ^
+    BLS,                 // <<
+    BRS,                 // >>
+    DOT,                 // .  
+    COMMA,               // ,
+    COLON,               // :
+    AT,                  // @
+
+    // comparison
+    EQEQ,                // ==
+    NEQ,                 // !=
+    LT,                  // <
+    GT,                  // >
+    LTE,                 // <=
+    GTE,                 // >=
+
+    // arithmetic
+    EQ,                  // =
+    PLUS,                // +
+    MIN,                 // -
+    DIV,                 // /
+    EXP,                 // **
+    MULT,                // * 
+    PLUSEQ,              // +=
+    MINEQ,               // -=
+    DIVEQ,               // /=
+    MULTEQ,              // *=
 };
 
 struct Token {
@@ -100,6 +95,29 @@ struct Token {
     std::shared_ptr<Token> next = nullptr;
 };
 
+static std::unordered_map<Type, std::string> TOKEN_EXPR = {
+    {Type::MLINE_COMMENT, R"(#\*[^*]*\*#)"},
+    {Type::SLINE_COMMENT, R"(#([^\n]*)(\n|$))"},
+    {Type::STRING, R"("(\\.|[^"\\])*"|'(\\.|[^'\\])*')"},
+    {Type::WHITESPACE, R"([ \t]+)"},
+    {Type::NLINE, R"(\r?\n)"},
+    {Type::SMBRACKET_L, R"(\()"},
+    {Type::SMBRACKET_R, R"(\))"},
+    {Type::SQBRACKET_L, R"(\[)"},
+    {Type::SQBRACKET_R, R"(\])"},
+    {Type::CUBRACKET_L, R"(\{)"},
+    {Type::CUBRACKET_R, R"(\})"},
+    {Type::EQ, R"(=)"},
+    {Type::PLUS, R"(\+)"},
+    {Type::MIN, R"(\-)"},
+    {Type::DIV, R"(\/)"},
+    {Type::EXP, R"(\^)"},
+    {Type::MULT, R"(\*)"},
+    {Type::DEF, R"(\bdef\b)"},
+    {Type::RET, R"(\bret\b)"},
+};
+
+/*
 static std::unordered_map<Type, std::string> TOKEN_EXPR = {
     {Type::MLINE_COMMENT, ""},
     {Type::SLINE_COMMENT, R"(#([^\n]*)(\n|$))"},
@@ -120,6 +138,6 @@ static std::unordered_map<Type, std::string> TOKEN_EXPR = {
     {Type::MULT, R"(\*)"},
     {Type::DEF, R"(\bdef\b)"},
     {Type::RET, R"(\bret\b)"},
-};
+};*/
 
-}  // namespace Tok
+}  // namespace Tokenizer
